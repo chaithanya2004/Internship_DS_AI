34bdb68afe05143a77b0d5d860e35c79c92c6bf9
chaithanya <116102108+chaithanya2004@users.noreply.github.com>
2026-02-03 23:31:13 +0530

    Add files via upload

---
+++ Day3/day3_task2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  3 23:11:48 2026
+
+@author: User
+"""
+
+temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+print("First index");
+print(temperatures[0]);
+print("last index");
+print(temperatures[-1]);
+print("Afternoon peak")
+print(temperatures[3:6]);
+print("Last 3 hours")
+print(temperatures[-3:]);
---
+++ Day3/day3_task3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  3 23:23:16 2026
+
+@author: User
+"""
+
+screen_res = (1920, 1080);
+print(f"Current Resolution:{screen_res[0]}X{screen_res[-1]}")
+#Tuples cannot be modified
+screen_res[0] = 1280
