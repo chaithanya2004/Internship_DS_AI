428f071634f05cb5821d1cc924f32e31570bee77
chaithanya <116102108+chaithanya2004@users.noreply.github.com>
2026-02-03 23:31:49 +0530

    Add files via upload

---
+++ Day3/day3_task1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  3 23:01:42 2026
+
+@author: User
+"""
+
+inventory=["Apple","Bananas","Carrots","Dates"];
+inventory=["Apple","Bananas","Carrots","Dates"];
+inventory.append("Eggs");
+inventory.remove("Bananas");
+inventory.sort();
+print(inventory);
